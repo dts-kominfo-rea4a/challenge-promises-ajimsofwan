9703ed2703f6811f7baf237f7c4493a754297d30
Aji Muhammad Sofwan <ajimsofwan@gmail.com>
2022-09-16 01:35:19 +0700

    feat: modifikasi promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (parameter) => {
+  const merged = [...await promiseTheaterIXX(), ...await promiseTheaterVGC()];
+  let counter = 0;
+  merged.forEach(hitung => {
+    if (hitung.hasil === parameter) {
+      counter++;
+    }
+  });
+  return counter;
+};
 
 module.exports = {
   promiseOutput,
